2a7ecb3890c145766df09d8eccc9f02a7c9c70b3
Yvens 16 <yvensbelaston@gmail.com>
2018-08-22 00:01:27 +0200

    Broco finally bounce of player

---
+++ intro.js
@@ -0,0 +1,11 @@
+// class intro extends Phaser.Scene {
+//   constructor(){
+//     super ({key:"Intro"});
+//   }
+//   preload(){
+//     this.load.image('Intro','./fat-bobby.png');
+//   }
+//   create(){
+//     this.add.image(600, 800,'Intro');
+//   }
+// }
--- main.js
+++ main.js
@@ -19,6 +19,15 @@ var config = {
 var player;
 var platforms;
 var burger;
+var score = 0;
+var scoreText;
+
+var burgers = [];
+var speed = 5;
+
+
+
+
 
 var game = new Phaser.Game(config);
 
@@ -26,6 +35,7 @@ function preload (){
   this.load.image('back', './2-31.jpg');
   this.load.image('ran', './frame-2.png');
   this.load.image('burger', './burger.png');
+  this.load.image('broco', './Broccoli.png');
   // this.load.spritesheet('run', './spritesheet.png', { frameWidth: 64, frameHeight: 48 });
   this.load.image('ground',"./platform.png");
 
@@ -43,9 +53,9 @@ function create (){
   platforms.create(300, 798, 'ground').setScale(2).refreshBody();
   this.alpha=0,1;
 
-player = this.physics.add.sprite(100, 650, 'ran').setScale(0.4).setInteractive();
+player = this.physics.add.sprite(100, 650, 'ran').setScale(0.3).setInteractive();
 player.setBounce(0.2);
-player.setCollideWorldBounds(true);
+player.setCollideWorldBounds();
 
 
 this.physics.add.collider(player, platforms);
@@ -74,7 +84,7 @@ this.input.on('dragend', function (pointer, gameObject){
 //------------- FALLING BURGER OBJ----------------------
 burger = this.physics.add.group({
   key:'burger',
-  repeat:500,
+  repeat:4,
   setXY: {x:80, y:0, stepX: 120 },
   setScale: { x: 0.1, y: 0.1, stepY: 0 }
 });
@@ -87,8 +97,40 @@ this.physics.add.overlap(player, burger, collectBurger, null,this);
 
 function collectBurger(player, burger){
   burger.disableBody(true, true);
+  score +=10;
+  scoreText.setText('Score: ' + score);
 }
 //------------- FALLING BURGER OBJ----------------------
+
+//------------- FALLING Brocolli OBJ----------------------
+broco = this.physics.add.group({
+  key:'broco',
+  repeat:4,
+  setXY: {x:80, y:0, stepX: 120 },
+  setScale: { x: 0.1, y: 0.1, stepY: 0},
+});
+
+broco.children.iterate(function (child){
+  child.setBounceY(Phaser.Math.FloatBetween(0.2, 0.7));
+
+});
+//-----------------
+
+//-------------
+this.physics.add.collider(broco, player, hitBroco);
+
+function hitBroco (player, broco){
+  broco.body.bounce.setTo(2,2);
+  // player.body.bounce.setTo(1,1);
+  // bomb.setCollideWorldBounds(true);
+  // broco.setVelocity(Phaser.Math.Between(-900,-900), 0);
+  broco.setVelocity(-200);
+  score -=10;
+  scoreText.setText('score: ' +score);
+}
+//------------- FALLING Brocolli OBJ----------------------
+
+scoreText = this.add.text(16,16, 'score:0', {fontSize: '32px', fill: 'red'});
 
 }
 
