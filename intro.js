220c8a32df06115de86047484318ebdcd5f11ada
Yvens 16 <yvensbelaston@gmail.com>
2018-08-22 00:36:44 +0200

    just saving before big change

--- intro.js
+++ intro.js
@@ -1,11 +1,17 @@
-// class intro extends Phaser.Scene {
-//   constructor(){
-//     super ({key:"Intro"});
-//   }
-//   preload(){
-//     this.load.image('Intro','./fat-bobby.png');
-//   }
-//   create(){
-//     this.add.image(600, 800,'Intro');
-//   }
-// }
+class intro extends Phaser.Scene {
+  constructor(){
+    super ({key:"Intro"});
+  }
+  preload(){
+    this.load.image('Intro','./fat-bobby.png');
+  }
+  create(){
+    this.add.image(600, 800,'Intro');
+
+    this.input.keybord.on('keyup',function(e){
+      if(e.key =="13"){
+        this.scene.start("Game")
+      }
+    }, this)
+  }
+}
